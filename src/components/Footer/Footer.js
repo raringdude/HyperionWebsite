bf52cf1ac12ae02c8d749dc5fc5ac40f179a403c
James Guidi <jamesguidi@jamess-mbp-2.lan>
2025-03-13 16:35:45 -0400

    Made Facebook and Linkedin Links Work in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,8 +12,8 @@ const Footer = () => {
                 </div>
                 <div className="footer-section">
                     <h3>Find Us</h3>
-                    <p>Facebook</p>
-                    <p>LinkedIn</p>
+                    <a href="https://www.facebook.com/profile.php?id=61573758227504&mibextid=wwXIfr&rdid=bxuLU6iWBZyRefEy&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F15tnSsiVhc%2F%3Fmibextid%3DwwXIfr#" target="_blank" rel="noopener noreferrer"><p>Facebook</p></a>
+                    <a href="https://www.linkedin.com/company/hyperion-residential/about/?viewAsMember=true" target="_blank" rel="noopener noreferrer"><p>LinkedIn</p></a>
                     
                 </div>
                 <div className="footer-section">
