13296839a7fe8a5bfe4fba9fe2f1cf53dafdaa00
James Guidi <jamesguidi@jamess-mbp-2.lan>
2025-03-15 14:13:47 -0400

    Always show the top of the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Section from './components/Section/Section';
@@ -13,6 +14,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="App">
         <Navbar />
         <Routes>
