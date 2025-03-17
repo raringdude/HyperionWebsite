271d7d402e068094760a76e0b93b7bfd6f8721db
James Guidi <jamesguidi@Jamess-MacBook-Pro-2.local>
2025-03-16 19:43:43 -0500

    added jerry and fixed nav bar

--- src/pages/OurPeople.js
+++ src/pages/OurPeople.js
@@ -6,6 +6,7 @@ import '../styles/OurPeople.css';
 import jamesImage from '../components/Assets/Headshots/JamesG_Headshot.jpeg';
 import stevenImage from '../components/Assets/Headshots/SteveD_Headshot.JPG';
 import andrienImage from '../components/Assets/Headshots/AndrienW_Headshot.PNG';
+import jerryImage from '../components/Assets/Headshots/JerryW_Headshot.jpg';
 
 const OurPeople = () => {
     return (
@@ -13,7 +14,11 @@ const OurPeople = () => {
             <h1>Our People</h1>
             <div className="people-grid">
                 <div className="person-card">
-                    <div className="person-image placeholder"></div>
+                <img 
+                        src={jerryImage} 
+                        alt="Jerry Wojenski" 
+                        className="person-image"
+                    />
                     <h3>Jerry Wojenski</h3>
                     <p>CEO</p>
                 </div>
